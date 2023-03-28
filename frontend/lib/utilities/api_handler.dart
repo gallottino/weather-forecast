579ea40bce9cfc6a26a082943c1ef6ce3147a172
Davide Gallotti <d.gallotti@outlook.it>
2023-03-28 23:35:54 +0200

    Mock with city name and days navigation
    Signed-off-by: Davide Gallotti <d.gallotti@outlook.it>

--- frontend/lib/utilities/api_handler.dart
+++ frontend/lib/utilities/api_handler.dart
@@ -3,10 +3,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiHandler {
-  static getForecast(double latitude, double longitude) async {
+  static getForecast(String city) async {
     try {
       var url = Uri.parse(
-          'http://localhost:3000?lat=45.0703&lon=7.6869&units=metric');
+          'http://localhost:3000?city=$city');
       var res = await http.get(url);
       if(res.statusCode == 200) {
         Map<String, dynamic> days = json.decode(res.body);
